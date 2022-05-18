63365722db30becc7187a349ed0cb205e44c1141
C B Sajan <sajan@ckhathri.com>
2022-05-18 12:20:23 +0530

    05-Vee-validate-pt-one

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <app-header></app-header>
   <div class="container">
-    <comp-contact></comp-contact>
+    <!-- <comp-contact></comp-contact> -->
+    <comp-Validate> </comp-Validate>
   </div>
   <app-footer></app-footer>
 </template>
 
 <script>
-import compContact from './components/Contact/index_comp.vue';
+// import compContact from './components/Contact/index_comp.vue';
+import compValidate from './components/Validate/Index-Comp.vue';
 export default {
   components: {
-    compContact
+    // compContact
+    compValidate
   }
 }
 </script>
